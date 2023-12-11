46711bbdff97890f1086529e629813f3c3ffeb9a
Артем <babenkoartem505@gmail.com>
2023-12-11 19:27:00 +0200

    Controllers

---
+++ Controllers/PageController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RecipeWebApp.Controllers;
+
+public class PageController : Controller
+{
+    /// <summary>
+    /// Надсилає головну сторінку.
+    /// </summary>
+    [HttpGet]
+    [Route("/")]
+    public IActionResult GetMainPage()
+    {
+        return File("html/index.html", "text/html; charset=utf-8;");
+    }
+}
---
+++ Controllers/RecipeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RecipeWebApp.Models;
+
+namespace RecipeWebApp.Controllers;
+
+public class RecipeController : Controller
+{
+    RecipeDbContext db;
+    public RecipeController(RecipeDbContext db) => this.db = db;
+
+    /// <summary>
+    /// Метод, який повертає рецепти з бази даних.
+    /// </summary>
+    /// <returns>Дані про всі рецепти у вигляді JSON.</returns>
+    [HttpGet]
+    [Route("/recipes")]
+    public async Task<IActionResult> GetRecipes()
+    {
+        var recipes = await db.Recipes.ToListAsync();
+        return Json(recipes); 
+    }
+}
