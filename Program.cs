83993c86c45f76fa285812e72f4ac74329c771b4
Артем <babenkoartem505@gmail.com>
2023-12-11 19:28:34 +0200

    Program

--- Program.cs
+++ Program.cs
@@ -1,6 +1,36 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
 
-app.Run();
+using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.EntityFrameworkCore;
+using RecipeWebApp.Models;
+
+class Program
+{
+    static void Main()
+    {
+        WebApplicationBuilder builder = WebApplication.CreateBuilder();
+        builder.Services.AddControllers();
+        builder.Services.AddDbContext<RecipeDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+        builder.Services.AddResponseCompression(options =>
+        {
+            options.EnableForHttps = true;
+            options.Providers.Add<GzipCompressionProvider>();
+            options.Providers.Add<BrotliCompressionProvider>();
+            options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
+            {
+                "text/html",
+                "text/css",
+                "text/javascript",
+                "application/json"
+            });
+        });
+        WebApplication app = builder.Build();
+
+        app.UseStaticFiles();
+        app.UseResponseCompression();
+
+        app.MapControllers();
+
+        app.Run();
+    }
+}
