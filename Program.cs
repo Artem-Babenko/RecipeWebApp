6deca587b2b82e4848fa628a9aeb9603e9ba7d82
Babenko Artem <babenkoartem505@gmail.com>
2024-01-12 15:14:31 +0200

    Program

--- Program.cs
+++ Program.cs
@@ -5,49 +5,55 @@ using Microsoft.EntityFrameworkCore;
 using RecipeWebApp.Models;
 using RecipeWebApp.Services;
 
-class Program
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder();
+// Додавання служби для очиження тимчасовий фото.
+builder.Services.AddHostedService<PhotoCleanupService>();
+// Додавання контролерів.
+builder.Services.AddControllers();
+// Додавання контексту бази даних.
+builder.Services.AddDbContext<RecipeDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
+// Додавання стиснення відповіді.
+builder.Services.AddResponseCompression(options =>
 {
-    static void Main()
+    options.EnableForHttps = true;
+    options.Providers.Add<GzipCompressionProvider>();
+    options.Providers.Add<BrotliCompressionProvider>();
+    options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder();
-        // Додавання служби для очиження тимчасовий фото.
-        builder.Services.AddHostedService<PhotoCleanupService>();
-        // Додавання контролерів.
-        builder.Services.AddControllers();
-        // Додавання контексту бази даних.
-        builder.Services.AddDbContext<RecipeDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DevConnection")));
-        // Додавання стиснення відповіді.
-        builder.Services.AddResponseCompression(options =>
-        {
-            options.EnableForHttps = true;
-            options.Providers.Add<GzipCompressionProvider>();
-            options.Providers.Add<BrotliCompressionProvider>();
-            options.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(new[]
-            {
-                "text/html",
-                "application/json"
-            });
-        });
-        // Додавання аутентифікації.
-        builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
-        {
-            options.Cookie.HttpOnly = false;
-            options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
-            options.Cookie.Name = "AuthenticationCookie";
-            options.AccessDeniedPath = "/login";
-            options.LoginPath = "/login";
-        });
-        // Додавання авторизації.
-        builder.Services.AddAuthorization();
-        WebApplication app = builder.Build();
+        "text/html",
+        "application/json"
+    });
+});
+// Додавання аутентифікації.
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+{
+    options.Cookie.HttpOnly = false;
+    options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
+    options.Cookie.Name = "AuthenticationCookie";
+    options.AccessDeniedPath = "/login";
+    options.LoginPath = "/login";
+});
+// Додавання авторизації.
+builder.Services.AddAuthorization();
+// Додавання кешування.
+builder.Services.AddMemoryCache();
+// Додавання сервісу Swagger.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+WebApplication app = builder.Build();
 
-        app.UseStaticFiles(); 
-        app.UseAuthentication();  
-        app.UseAuthorization();   
-        app.UseResponseCompression();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseStaticFiles(); 
+app.UseAuthentication();  
+app.UseAuthorization();   
+app.UseResponseCompression();
+
+app.MapControllers();
+
+app.Run();
